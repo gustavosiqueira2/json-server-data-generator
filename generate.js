038608dea0072b7e5beb4a2b919967dc1516f7ca
gustavooo1 <gustavo.fariassiqueira@gmail.com>
2020-07-26 23:32:18 -0300

    performance and start to build the cli

--- generate.js
+++ generate.js
@@ -1,84 +1,78 @@
 const fs = require('fs');
 
-let db = {
-  users: [],
-  histories: [],
-  chapter: [],
-  likedHistories: []
-}
+// Example of property that goes in the 'database' variable
+// 
+// user: {
+//   array: [],
+//   quantity: 3,
+//   obj: {
+//     id: 'number',
+//     name: 'string',
+//     description: 'string'
+//   }
+// }
 
-const numberOfRows = {
-  users: 3,
-  histories: 10,
-  chapter: 40,
-  likedHistories: 60
-}
+// Carry the information to generate data and the data it self
+let database = {}
 
-const classes = {
-  users: {
-    id: 'number',
-    name: 'string',
-    description: 'string'
-  },
-  histories: {
-    userId: 'number',
-    id: 'number',
-    name: 'string',
-    description: 'string'
-  },
-  chapter: {
-    historyId: 'number',
-    id: 'number',
-    name: 'string',
-    description: 'string',
-    text: 'string'
-  },
-  likedHistories: {
-    userId: 'number',
-    historyId: 'number'
+// Generate data for specific entity of the database
+function generateRows(clas, objectName) {
+
+  // create the specifed quantity of data per entity
+  for (let i = 0; i < clas.quantity; i++) {
+
+    // creating row and adding to the array
+    let createdObject = createObject(clas.obj, objectName);
+    database[objectName].array.push(createdObject);
   }
 }
 
-function generateQuantity(qnt, obj) {
-  for (let i = 0; i < qnt; i++) {
-    let res = generateData(classes[obj], obj);
-    db[obj].push(res);
-  }
-}
-
-function generateData(obj, name) {
+// Creates the object that will be added to the array
+function createObject(object, objectName) {
   let res = {};
-  Object.entries(obj).forEach((propertie) => {
+  Object.entries(object).forEach((propertie) => {
     if (propertie[1] == 'number')
       if (propertie[0] == 'id')
-        res[propertie[0]] = db[name].length + 1;
+        res[propertie[0]] = database[objectName].array.length + 1;
       else {
         if (propertie[0].split('Id').length == 2)
-          res[propertie[0]] = Math.floor(Math.random() * (numberOfRows[name] - 1) + 1);
+          res[propertie[0]] = Math.floor(Math.random() * (database[propertie[0].split('Id')[0]].quantity - 1) + 1);
         else
           res[propertie[0]] = Math.floor(Math.random() * (100 - 1) + 1);
       }
-    else
+    else {
       if (propertie[0] == 'name')
         res[propertie[0]] = 'AAAAAA';
       else {
         if (propertie[0] == 'text')
-          res[propertie[0]] = 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC';
+          res[propertie[0]] = 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC';
         else
           res[propertie[0]] = 'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB';
       }
+    }
   });
   return res;
 }
 
-const generate = () => {
+// Generate the data base
+const generate = async () => {
 
-  Object.keys(db).forEach((clas) => generateQuantity(numberOfRows[clas], clas));
+  // variable that keep the final json
+  const data = {};
 
-  const data = JSON.stringify(db);
+  await Object.keys(database).forEach(async (clas) => {
 
-  fs.writeFile('server.json', data, 'utf8', () => { });
+    // generate all rows
+    await generateRows(database[clas], clas);
+
+    // add to final array
+    data[clas] = database[clas].array;
+  });
+
+  // creates the file with the data
+  fs.writeFile('server.json', JSON.stringify(data), 'utf8', () => { });
 
 }
 
+// run 
 generate();
---
+++ index.js
@@ -0,0 +1,9 @@
+const readlineimport = require('readline');
+
+const readline = readlineimport.createInterface({ input: process.stdin, output: process.stdout });
+
+readline.question(`
+  \x1b[1m\x1b[32mWelcome to json-server-data-generator\x1b[0m
+  \x1b[1m\x1b[4mWhat you wanna do? (help to see the commands)\x1b[0m
+  Actual version 0.1
+`, (res) => commands()[res](rl));
