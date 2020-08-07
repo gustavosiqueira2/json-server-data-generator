e7c40caba4c2d073f26c3ad4c53d42c4351917d0
gustavooo1 <gustavo.fariassiqueira@gmail.com>
2020-08-06 21:35:57 -0300

    '

--- types/types.js
+++ types/types.js
@@ -1,9 +1,9 @@
 module.exports = ([name, propertie], database) => {
 
   const types = {
-    name: 'AAAAA',
-    string: 'BBBBBBBBBBBBBBBBBBB',
-    text: 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC',
+    name: 'Name Teste',
+    string: 'template string',
+    text: 'template text',
     number: name.split('Id').length == 2 ?
       Math.floor((Math.random() * (database[name.split('Id')[0]].quantity - 1)) + 1) :
       Math.floor(Math.random() * (100 - 1) + 1)
